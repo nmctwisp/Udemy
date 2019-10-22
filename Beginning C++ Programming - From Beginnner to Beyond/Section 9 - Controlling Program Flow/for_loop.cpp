fbb18fca71e2e7aefb657705409b70fd2e94ee6e
nmctwisp <chen.k.yam@gmail.com>
2019-10-22 11:54:19 -0400

    added prefix and postfix incrementation examples to for_loop.cpp

--- Beginning C++ Programming - From Beginnner to Beyond/Section 9 - Controlling Program Flow/for_loop.cpp
+++ Beginning C++ Programming - From Beginnner to Beyond/Section 9 - Controlling Program Flow/for_loop.cpp
@@ -2,7 +2,14 @@
 
 using namespace std;
 
-int for_loop_example(int max_iteration) {
+int for_loop_pre(int max_iteration) {
+    for (int i = 1; i <= max_iteration; ++i) {
+        cout << i << endl;
+    }
+    return NULL;
+}
+
+int for_loop_post(int max_iteration) {
     for (int i = 1; i <= max_iteration; i++) {
         cout << i << endl;
     }
@@ -15,6 +22,11 @@ int main() {
     cout << "Enter the max number of iterations: ";
     cin >> max;
 
-    for_loop_example(max);
+    cout << "Prefix Increment Loop" << endl;
+    cout << "=====================" << endl;
+    for_loop_pre(max);
+    cout << "Postfix Increment Loop" << endl;
+    cout << "=====================" << endl;
+    for_loop_post(max);
     return 0;
 }
