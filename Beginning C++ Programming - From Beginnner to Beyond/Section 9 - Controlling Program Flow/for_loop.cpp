f894b474db835fd4b514d35cf3dc6a8eaa590a92
nmctwisp <chen.k.yam@gmail.com>
2019-10-22 12:34:09 -0400

    added more loop examples with different data types

--- Beginning C++ Programming - From Beginnner to Beyond/Section 9 - Controlling Program Flow/for_loop.cpp
+++ Beginning C++ Programming - From Beginnner to Beyond/Section 9 - Controlling Program Flow/for_loop.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
@@ -16,6 +18,26 @@ int for_loop_post(int max_iteration) {
     return NULL;
 }
 
+int for_loop_arr(int arr[], int size) {
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << endl;
+    }
+    return NULL;
+}
+
+int for_loop_vec(vector<string> vec) {
+    for (int i = 0; i < vec.size(); ++i) {
+        cout << vec[i] <<endl;
+    }
+    return NULL;
+}
+
+int for_loop_multiple_var(int num1, int num2) {
+    for (int i = 0, j = 0; i < num1, j < num2; i++, j++) {
+        cout << "(" << i << "," << j << ")" << endl;
+    }
+    return NULL;
+}
 int main() {
 
     int max;
@@ -28,5 +50,17 @@ int main() {
     cout << "Postfix Increment Loop" << endl;
     cout << "=====================" << endl;
     for_loop_post(max);
+
+    int arr[] = {11,12,13,14,15};
+    for_loop_arr(arr, 5);
+    cout << endl;
+    
+    vector<string> vec {"aesad","bdasdad","ctasdasd"};
+    for_loop_vec(vec);
+    cout << endl;
+
+    int num1, num2; 
+    num1 = num2 = 5;
+    for_loop_multiple_var(num1, num2);
     return 0;
 }
