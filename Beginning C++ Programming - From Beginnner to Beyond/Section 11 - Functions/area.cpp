cb71ee8def9596d3898fcf9427e36350bbcc8e85
nmctwisp <chen.k.yam@gmail.com>
2019-10-23 16:42:46 -0400

    added section 11 exercises

---
+++ Beginning C++ Programming - From Beginnner to Beyond/Section 11 - Functions/area.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//----DO NOT MODIFY THE CODE ABOVE THIS LINE----
+//----WRITE YOUR FUNCTION PROTOTYPES BELOW THIS LINE----
+
+
+
+
+//----WRITE YOUR FUNCTION PROTOTYPES ABOVE THIS LINE----
+//----DO NOT MODIFY THE CODE BELOW THIS LINE----
+
+void area_calc() {
+    
+    //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
+    //----WRITE YOUR FUNCTION CALLS BELOW THIS LINE----
+    
+    
+    
+    
+    //----WRITE YOUR FUNCTION CALLS ABOVE THIS LINE----
+    //----DO NOT MODIFY THE CODE BELOW THIS LINE----
+    
+    cout << "The area of the square is " << square_area << "\n" << "The area of the rectangle is " << rectangle_area;
+}
+
+//----DO NOT MODIFY THE CODE ABOVE THIS LINE----
+//----WRITE YOUR FUNCTION DEFINITIONS BELOW THIS LINE----
+
+
+
+
+
+
+
