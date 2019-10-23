0bd1463b73af1da95dc94b729fde0b394e60f58f
nmctwisp <chen.k.yam@gmail.com>
2019-10-23 16:03:41 -0400

    completed c_string exercise

--- Beginning C++ Programming - From Beginnner to Beyond/Section 10 - Characters and Strings/c_style_str.cpp
+++ Beginning C++ Programming - From Beginnner to Beyond/Section 10 - Characters and Strings/c_style_str.cpp
@@ -6,7 +6,18 @@ void strings_and_functions() {
     
     //----WRITE YOUR CODE BELOW THIS LINE----
     
+    char first_name[25] = "Bjarne";
+    char last_name[25] = "Stroustrup";
+    char whole_name[25];
+
+    size_t first_name_length = strlen(first_name);
+    size_t last_name_length = strlen(last_name);
+
     
+    strcpy(whole_name, first_name);
+    strcat(whole_name, last_name);
+    
+    size_t whole_name_length = strlen(whole_name);
     
     //----WRITE YOUR CODE ABOVE THIS LINE----
     //----DO NOT MODIFY THE CODE BELOW THIS LINE----
