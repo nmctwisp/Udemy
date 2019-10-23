22dc9c7b843793c580f329c6050fbfb824ba0afc
Chen Yam <chenyam@test.com>
2019-10-22 21:41:02 -0400

    completed while_loop exercise

--- Beginning C++ Programming - From Beginnner to Beyond/Section 9 - Controlling Program Flow/while_loop.cpp
+++ Beginning C++ Programming - From Beginnner to Beyond/Section 9 - Controlling Program Flow/while_loop.cpp
@@ -2,11 +2,24 @@
 #include <vector>
 using namespace std;
 
-int count_numbers(const vector<int> &vec) {
+int count_numbers( const vector<int> &vec) {
     //---- WRITE YOUR CODE BELOW THIS LINE----
     
-    
+    int count = 0;
+    size_t index = 0;
+
+    while (index < vec.size() && vec.at(index) != -99) {
+        ++count;
+        ++index;
+        cout << vec.at(index) << endl; 
+    }
     //---- WRITE YOUR CODE ABOVE THIS LINE----
-    //---- DO NOT MODIFY THE CODE BELOW THIS LINE-----
+    //---- DO NOT MODIFY THE CODE BELOW THIS LINE----
     return count;
+}
+
+int main() {
+    vector<int> vec {1,3,5,15,16,17,18,19,20,21,25,26,27,30,50,55,56,58,100,200,300,400,-99,500,600,700};
+
+    cout << count_numbers(vec);
 }
