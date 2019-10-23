a6e4a131d89ba944277e764497d1a88934faf3c5
Chen Yam <chenyam@test.com>
2019-10-22 22:28:56 -0400

    completed sum_of_product exercise

--- Beginning C++ Programming - From Beginnner to Beyond/Section 9 - Controlling Program Flow/sum_of_product.cpp
+++ Beginning C++ Programming - From Beginnner to Beyond/Section 9 - Controlling Program Flow/sum_of_product.cpp
@@ -1,11 +1,25 @@
 #include <vector>
+#include <iostream>
 using namespace std;
 
 int calculate_pairs(vector<int> vec) {
     //----WRITE YOUR CODE BELOW THIS LINE----
-    
-     
+    int result = 0;
+    for (size_t i=0; i < vec.size(); ++i) {
+        for (size_t j=i+1; j < vec.size(); ++j){
+            cout << vec.at(i) << " " << vec.at(j) << endl;
+            result += vec.at(i) * vec.at(j);
+        }
+    }
     //----WRITE YOUR CODE ABOVE THIS LINE----
     //----DO NOT MODIFY THE CODE BELOW THIS LINE----
     return result;
+}
+
+int main() {
+    vector<int> vec = {2, 4, 6, 8};
+
+    cout << calculate_pairs(vec);
+
+    return 0;
 }
