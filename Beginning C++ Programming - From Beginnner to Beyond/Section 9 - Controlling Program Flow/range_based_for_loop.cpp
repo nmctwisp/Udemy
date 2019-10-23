4e5879d20a399c2535e2356d1795e8d490aaf164
Chen Yam <chenyam@test.com>
2019-10-22 21:19:07 -0400

    completed range_base_loop exercise

--- Beginning C++ Programming - From Beginnner to Beyond/Section 9 - Controlling Program Flow/range_based_for_loop.cpp
+++ Beginning C++ Programming - From Beginnner to Beyond/Section 9 - Controlling Program Flow/range_based_for_loop.cpp
@@ -1,14 +1,26 @@
 #include <vector>
+#include <iostream>
 using namespace std;
 
 int count_divisible() {
     
-    vector<int> vec {1,3,5,15,16,17,18,19,20,21,25,26,27,30,50,55,56,58,100,200,300,400,500,600,700};
+    vector<int> vec {1,3,5,15,16,17,18,19,20,21,25,26,27,30,50,55,56,58,100,200,300,400,-99,500,600,700};
     //---- WRITE YOUR CODE BELOW THIS LINE----
-    
+
+    int count = 0;
+
+    for (auto elm: vec) {
+        if (elm % 3 == 0 || elm % 5 == 0)
+            count += 1;
+    }    
     
     
     //---- WRITE YOUR CODE ABOVE THIS LINE----
     //---- DO NOT CHANGE THE CODE BELOW THIS LINE----
     return count;
+}
+
+int main() {
+    cout << count_divisible();
+    return 0;
 }
