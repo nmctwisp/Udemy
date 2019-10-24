e13f56a5fb84090e5f6326867b0ef1882395c695
Chen Yam <chenyam@test.com>
2019-10-24 01:01:34 -0400

    updated section 10 assignment

--- Beginning C++ Programming - From Beginnner to Beyond/Section 10 - Characters and Strings/assignment.cpp
+++ Beginning C++ Programming - From Beginnner to Beyond/Section 10 - Characters and Strings/assignment.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
 
+    string input_str;
+    cout << "Enter a string: ";
+    cin >> input_str;
+    // input_str = "ABC";
+
+    int max_palindrome_length = input_str.size() * 2 - 1;
+    int current_palindrome_length = 1;
+    string available_char{};
+
+    for (char letter: input_str){
+        available_char.push_back(letter);
+        int padding_size = (max_palindrome_length - current_palindrome_length) / 2;
+        string padding (padding_size, ' ');
+        string palindrome_str{};
+
+        for (int i=1; i<=current_palindrome_length; ++i) {
+            int index = current_palindrome_length % i;
+            
+            cout << current_palindrome_length << " % " << i << " = " << index << endl;
+            palindrome_str.push_back(available_char.at(index));
+        }
+        cout <<endl;
+        cout << padding + palindrome_str + padding << endl;
+        current_palindrome_length += 2;
+    }
+        
     return 0;
 }
