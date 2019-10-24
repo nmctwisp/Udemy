a13526b032f5ddccca40571e7fde847910ab9384
nmctwisp <chen.k.yam@gmail.com>
2019-10-24 12:37:58 -0400

    completed section 10 assignment

--- Beginning C++ Programming - From Beginnner to Beyond/Section 10 - Characters and Strings/assignment.cpp
+++ Beginning C++ Programming - From Beginnner to Beyond/Section 10 - Characters and Strings/assignment.cpp
@@ -10,25 +10,21 @@ int main() {
     cin >> input_str;
     // input_str = "ABC";
 
-    int max_palindrome_length = input_str.size() * 2 - 1;
-    int current_palindrome_length = 1;
+    size_t max_palindrome_length = input_str.size() * 2 - 1;
     string available_char{};
 
     for (char letter: input_str){
         available_char.push_back(letter);
-        int padding_size = (max_palindrome_length - current_palindrome_length) / 2;
-        string padding (padding_size, ' ');
-        string palindrome_str{};
+        size_t available_char_len = available_char.length();
+        string palindrome_str = available_char;
 
-        for (int i=1; i<=current_palindrome_length; ++i) {
-            int index = current_palindrome_length % i;
-            
-            cout << current_palindrome_length << " % " << i << " = " << index << endl;
-            palindrome_str.push_back(available_char.at(index));
+        for (size_t i=available_char_len-2; i != -1; --i) {
+            palindrome_str.push_back(available_char.at(i));
         }
-        cout <<endl;
-        cout << padding + palindrome_str + padding << endl;
-        current_palindrome_length += 2;
+
+        size_t padding_size = (max_palindrome_length - palindrome_str.length()) / 2;
+        string padding (padding_size, ' ');
+        cout << padding << palindrome_str << padding <<endl;
     }
         
     return 0;
