4148b471d14d706eca59d2be6a23c91ef04b41cc
Chen Yam <chenyam@test.com>
2019-10-23 21:30:08 -0400

    completed section 10 exercise 1

--- Beginning C++ Programming - From Beginnner to Beyond/Section 10 - Characters and Strings/exercise1.cpp
+++ Beginning C++ Programming - From Beginnner to Beyond/Section 10 - Characters and Strings/exercise1.cpp
@@ -8,8 +8,11 @@ void cpp_strings() {
     
     //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
     //----WRITE YOUR CODE BELOW THIS LINE----
+    string first_name(unformatted_full_name, 0, 7);
+    string last_name = unformatted_full_name.substr(7, 7);
     
-    
+    string formatted_full_name;
+    formatted_full_name = first_name + last_name.insert(0, " ");
     
     //----WRITE YOUR CODE ABOVE THIS LINE----
     //----DO NOT MODIFY THE CODE BELOW THIS LINE----
