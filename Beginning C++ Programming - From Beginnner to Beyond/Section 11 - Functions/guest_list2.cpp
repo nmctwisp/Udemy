1e12292d3cfdeed3c83bc2224a78bba64972d7b3
nmctwisp <chen.k.yam@gmail.com>
2019-10-25 13:04:23 -0400

    completed guest list 2 program

--- Beginning C++ Programming - From Beginnner to Beyond/Section 11 - Functions/guest_list2.cpp
+++ Beginning C++ Programming - From Beginnner to Beyond/Section 11 - Functions/guest_list2.cpp
@@ -6,8 +6,8 @@ using namespace std;
 //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
 //----WRITE THE FUNCTION PROTOTYPES BELOW THIS LINE----
 
-string print_guest_list(            );//----WRITE THE FUNCTION PARAMETER LIST WITHIN THE PARENTHESES
-void clear_guest_list(            );//----WRITE THE FUNCTION PARAMETER LIST WITHIN THE PARENTHESES
+string print_guest_list(const string &guest_1, const string &guest_2, const string &guest_3);//----WRITE THE FUNCTION PARAMETER LIST WITHIN THE PARENTHESES
+void clear_guest_list(string &guest_1, string &guest_2, string &guest_3);//----WRITE THE FUNCTION PARAMETER LIST WITHIN THE PARENTHESES
 
 //----WRITE THE FUNCTION PROTOTYPES ABOVE THIS LINE----
 //----DO NOT MODIFY THE CODE BELOW THIS LINE----
@@ -20,9 +20,9 @@ void event_guest_list() {
 
     //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
     //----WRITE THE FUNCTION CALLS BELOW THIS LINE----
-
-    
-    
+    print_guest_list(guest_1, guest_2, guest_3);
+    clear_guest_list(guest_1, guest_2, guest_3);
+    print_guest_list(guest_1, guest_2, guest_3);
     
 
     //----WRITE THE FUNCTION CALLS ABOVE THIS LINE----
@@ -32,11 +32,13 @@ void event_guest_list() {
 //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
 //----WRITE THE FUNCTION DEFINITION BELOW THIS LINE----
 
-string print_guest_list(            ) {//----WRITE THE FUNCTION PARAMETER LIST WITHIN THE PARENTHESES
+string print_guest_list(const string &guest_1, const string &guest_2, const string &guest_3) {//----WRITE THE FUNCTION PARAMETER LIST WITHIN THE PARENTHESES
 
     //----WRITE THE FUNCTION BODY BELOW THIS LINE----
     
-    
+    cout << guest_1 << endl;
+    cout << guest_2 << endl;
+    cout << guest_3 << endl;
     
     //----WRITE THE FUNCTION BODY ABOVE THIS LINE----
     //----DO NOT MODIFY THE CODE BELOW THIS LINE----
@@ -47,12 +49,12 @@ string print_guest_list(            ) {//----WRITE THE FUNCTION PARAMETER LIST W
 //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
 //----WRITE THE FUNCTION DEFINITION BELOW THIS LINE----
 
-void clear_guest_list(            ) {//----WRITE THE FUNCTION PARAMETER LIST WITHIN THE PARENTHESES
+void clear_guest_list(string &guest_1, string &guest_2, string &guest_3) {//----WRITE THE FUNCTION PARAMETER LIST WITHIN THE PARENTHESES
     
     //----WRITE THE FUNCTION BODY BELOW THIS LINE----
-    
-    
-        
+    guest_1 = " ";
+    guest_2 = " ";
+    guest_3 = " ";    
     //----WRITE THE FUNCTION BODY ABOVE THIS LINE----
     //----DO NOT MODIFY THE CODE BELOW THIS LINE----
 }
