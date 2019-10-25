3a3267fd057cbbf2d512a9cadb511104a5f3c729
Chen Yam <chenyam@test.com>
2019-10-24 21:37:33 -0400

    completed converting temperature program

--- Beginning C++ Programming - From Beginnner to Beyond/Section 11 - Functions/converting_temp.cpp
+++ Beginning C++ Programming - From Beginnner to Beyond/Section 11 - Functions/converting_temp.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
 //----WRITE YOUR FUNCTION PROTOTYPES BELOW THIS LINE----
-void fahrenheit_to_celsius(double fahrenheit_temperature);
-void celsius_to_fahrenheit(double fahrenheit_temperature);
+double fahrenheit_to_celsius(double fahrenheit_temperature);
+double fahrenheit_to_kelvin(double fahrenheit_temperature);
 
 //----WRITE YOUR FUNCTION PROTOTYPES ABOVE THIS LINE----
 //----DO NOT MODIFY THE CODE BELOW THIS LINE----
@@ -15,7 +15,8 @@ void temperature_conversion(double fahrenheit_temperature) {
     //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
     //----WRITE YOUR FUNCTION CALLS BELOW THIS LINE----
     
-    
+    double celsius_temperature = fahrenheit_to_celsius(fahrenheit_temperature);
+    double kelvin_temperature = fahrenheit_to_kelvin(fahrenheit_temperature);
     
     //----WRITE YOUR FUNCTION CALLS ABOVE THIS LINE----
     //----DO NOT MODIFY THE CODE BELOW THIS LINE----
@@ -25,5 +26,15 @@ void temperature_conversion(double fahrenheit_temperature) {
 
 //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
 //----WRITE YOUR FUNCTION DEFINITIONS BELOW THIS LINE----
+
+double fahrenheit_to_celsius(double fahrenheit_temperature) {
+    double ctemp = (5.0/9.0) * (fahrenheit_temperature - 32);
+    return round(ctemp);
+}
+
+double fahrenheit_to_kelvin(double fahrenheit_temperature) {
+    double ktemp = fahrenheit_to_celsius(fahrenheit_temperature) + 273;
+    return round(ktemp);
+}
 
 
