f45f2759b9da5e039b111434eb08f9ce18eaf048
nmctwisp <chen.k.yam@gmail.com>
2019-10-25 17:56:58 -0400

    completed section challenge

--- Beginning C++ Programming - From Beginnner to Beyond/Section 11 - Functions/challenge.cpp
+++ Beginning C++ Programming - From Beginnner to Beyond/Section 11 - Functions/challenge.cpp
@@ -3,6 +3,12 @@
 
 using namespace std;
 
+void print_numbers(const vector<int> vec);
+void add_numbers(vector<int> &vec);
+double get_mean(const vector<int> vec);
+int min(const vector<int> vec);
+int max(const vector<int> vec);
+
 int main() {
     vector<int> vec;
     char choice;
@@ -20,57 +26,24 @@ int main() {
         switch (choice) {
             case 'p':
             case 'P':
-                cout << "[ ";
-                for (size_t elm: vec)
-                    cout << elm << " ";
-                cout << "]" << endl;
+                print_numbers(vec);
                 break;
             case 'a':
-            case 'A': {
-                int elm;
-                cout << "Enter a number: ";
-                cin >> elm;
-                vec.push_back(elm);
+            case 'A':
+                add_numbers(vec);
                 break;
-            }
             case 'm': 
-            case 'M': {
-                int total;
-                for (size_t elm: vec)
-                    total += elm;
-                cout << static_cast<double>(total)/vec.size() << endl;
+            case 'M': 
+                cout << get_mean(vec) << endl;
                 break;
-            }
             case 's': 
-            case 'S': {
-                int smallest {0};
-                int current {0};
-                for (size_t i=0; i < vec.size(); ++i) {
-                    int index = i;
-                    current = vec.at(index);
-                    if (index == 0)
-                        smallest = current;
-                    else if (current < smallest)
-                        smallest = current;
-                }
-                cout << smallest << endl;
+            case 'S':
+                cout << min(vec) << endl;
                 break;   
-            }
             case 'l': 
-            case 'L': {
-                int largest {0};
-                int current {0};
-                for (size_t i=0; i < vec.size(); ++i) {
-                    int index = i;
-                    current = vec.at(index);
-                    if (index == 0)
-                        largest = current;
-                    else if (current > largest)
-                        largest = current;
-                }
-                cout << largest << endl;
+            case 'L': 
+                cout << max(vec) << endl;
                 break;
-            }
             default:
                 cout << "Goodbye!" << endl;         
         }
@@ -80,4 +53,53 @@ int main() {
     } while (choice != 'Q' && choice != 'q');
     
     return 0;
+}
+
+void print_numbers(const vector<int> vec) {
+    cout << "[ ";
+    for (int elm: vec)
+        cout << elm << " ";
+    cout << "]" << endl;
+}
+
+void add_numbers(vector<int> &vec) {
+    int elm;
+    cout << "Enter a number: ";
+    cin >> elm;
+    vec.push_back(elm);
+}
+
+double get_mean(const vector<int> vec) {
+    int total{};
+    for (int elm: vec)
+        total += elm;
+    return static_cast<double>(total)/vec.size();
+}
+
+int min(const vector<int> vec) {
+    int smallest {0};
+    int current {0};
+    for (size_t i=0; i < vec.size(); ++i) {
+        int index = i;
+        current = vec.at(index);
+        if (index == 0)
+            smallest = current;
+        else if (current < smallest)
+            smallest = current;
+    }
+    return smallest;
+}
+
+int max(const vector<int> vec) {
+    int largest {0};
+    int current {0};
+    for (size_t i=0; i < vec.size(); ++i) {
+        int index = i;
+        current = vec.at(index);
+        if (index == 0)
+            largest = current;
+        else if (current > largest)
+            largest = current;
+    }
+    return largest;
 }
