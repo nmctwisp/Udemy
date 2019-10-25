f94286bafe1e107c3a2f5b44201d2ecaef541b27
Chen Yam <chenyam@test.com>
2019-10-24 23:07:37 -0400

    completed guest list 1 program

--- Beginning C++ Programming - From Beginnner to Beyond/Section 11 - Functions/guest_list1.cpp
+++ Beginning C++ Programming - From Beginnner to Beyond/Section 11 - Functions/guest_list1.cpp
@@ -6,8 +6,8 @@ using namespace std;
 //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
 //----WRITE THE FUNCTION PROTOTYPES BELOW THIS LINE----
 
-string print_guest_list(            );//----WRITE THE FUNCTION PARAMETER LIST WITHIN THE PARENTHESES
-void clear_guest_list(            );//----WRITE THE FUNCTION PARAMETER LIST WITHIN THE PARENTHESES
+string print_guest_list(const string[], size_t);//----WRITE THE FUNCTION PARAMETER LIST WITHIN THE PARENTHESES
+void clear_guest_list(string[], size_t);//----WRITE THE FUNCTION PARAMETER LIST WITHIN THE PARENTHESES
                                       
 //----WRITE THE FUNCTION PROTOTYPES ABOVE THIS LINE----                                      
 //----DO NOT MODIFY THE CODE BELOW THIS LINE----
@@ -20,10 +20,10 @@ void event_guest_list() {
     //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
     //----WRITE THE FUNCTION CALLS BELOW THIS LINE----
 
+    print_guest_list(guest_list, guest_list_size);
+    clear_guest_list(guest_list, guest_list_size);
+    print_guest_list(guest_list, guest_list_size);    
     
-    
-    
-
     //----WRITE THE FUNCTION CALLS ABOVE THIS LINE----
     //----DO NOT MODIFY THE CODE BELOW THIS LINE----
 }
@@ -31,10 +31,11 @@ void event_guest_list() {
 //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
 //----WRITE THE FUNCTION DEFINITION BELOW THIS LINE----
 
-string print_guest_list(            ) {//----WRITE THE FUNCTION PARAMETER LIST WITHIN THE PARENTHESES
+string print_guest_list(const string guest_list[], size_t guest_list_size) {//----WRITE THE FUNCTION PARAMETER LIST WITHIN THE PARENTHESES
 
     //----WRITE THE FUNCTION BODY BELOW THIS LINE----
-    
+    for (size_t i{0}; i < guest_list_size; i++)
+        cout << guest_list[i] << endl;
     
     
         
@@ -46,13 +47,20 @@ string print_guest_list(            ) {//----WRITE THE FUNCTION PARAMETER LIST W
 //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
 //----WRITE THE FUNCTION DEFINITION BELOW THIS LINE----
 
-void clear_guest_list(            ) {//----WRITE THE FUNCTION PARAMETER LIST WITHIN THEPARENTHESES
+void clear_guest_list(string guest_list[], size_t guest_list_size) {//----WRITE THE FUNCTION PARAMETER LIST WITHIN THEPARENTHESES
 
     //----WRITE THE FUNCTION BODY BELOW THIS LINE----
-    
-    
+    for (size_t i=0; i<guest_list_size; ++i)
+        guest_list[i] = " ";    
     
     
     //----WRITE THE FUNCTION BODY ABOVE THIS LINE----
     //----DO NOT MODIFY THE CODE BELOW THIS LINE----
+}
+
+int main() {
+
+    event_guest_list();
+
+    return 0;
 }
