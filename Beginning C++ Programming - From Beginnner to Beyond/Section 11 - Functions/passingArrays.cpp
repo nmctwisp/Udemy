73abbe5847d5f847718529776af132cd0861deae
nmctwisp <chen.k.yam@gmail.com>
2019-10-25 12:45:48 -0400

    added comment to passingArrays explaining the difference in passing arrays to a function

--- Beginning C++ Programming - From Beginnner to Beyond/Section 11 - Functions/passingArrays.cpp
+++ Beginning C++ Programming - From Beginnner to Beyond/Section 11 - Functions/passingArrays.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main() {
+void print(const string arr[], size_t arr_len);
 
+
+int main() {
+    // Arrays are called by Reference instead of call by Value so you
+    // do not need to use the reference operator, & for the parameter list
+    string arr[] = {"Chen", "Yam"};
+    size_t arr_len = 2;
+    print(arr, arr_len);
     return 0;
+}
+
+void print(const string arr[], size_t arr_len) {
+    for (size_t i=0; i < arr_len; ++i)
+        cout << arr[i] << endl;
 }
