569086abd644c086e0d76a2e30f062ed229fd351
nmctwisp <chen.k.yam@gmail.com>
2019-10-25 12:46:08 -0400

    added an example of passing vectors to a function

--- Beginning C++ Programming - From Beginnner to Beyond/Section 11 - Functions/passingVectors.cpp
+++ Beginning C++ Programming - From Beginnner to Beyond/Section 11 - Functions/passingVectors.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int main() {
+void print1(vector<string> vec);
+void print2(vector<string> &vec);
 
+int main() {
+    vector<string> vec = {"Chen", "Yam"};
+    print1(vec);
+    cout << endl;
+    cout << "Main Function Values" << endl;
+    cout << "====================" << endl;
+    cout << vec[0] << endl;
+    cout << vec[1] << endl;
+    print2(vec);
+    cout << endl;
+    cout << "Main Function Values" << endl;
+    cout << "====================" << endl;
+    cout << vec[0] << endl;
+    cout << vec[1] << endl;
     return 0;
+}
+
+void print1(vector<string> vec) {
+    vec.at(0) = "Charles";
+    cout << "Print1 Function Values" << endl;
+    cout << "====================" << endl;
+    for (string name: vec) {
+        cout << name << endl;
+    }
+}
+
+void print2(vector<string> &vec) {
+    vec.at(0) = "Charles";
+    cout << "Print2 Function Values" << endl;
+    cout << "====================" << endl;
+    for (string name: vec) {
+        cout << name << endl;
+    }
 }
