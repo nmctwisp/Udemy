5251c17e912e008a446662eda4dc3eba5bb07f2e
Chen Yam <chenyam@test.com>
2019-10-24 22:06:08 -0400

    completed grocery list exercise

--- Beginning C++ Programming - From Beginnner to Beyond/Section 11 - Functions/groceries.cpp
+++ Beginning C++ Programming - From Beginnner to Beyond/Section 11 - Functions/groceries.cpp
@@ -4,7 +4,7 @@ using namespace std;
 //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
 //----WRITE YOUR FUNCTION PROTOTYPE BELOW THIS LINE----
 
-void print_grocery_list()
+void print_grocery_list(int apples=3, int oranges=7, int mangos=13);
 
 //----WRITE YOUR FUNCTION PROTOTYPE ABOVE THIS LINE----
 //----DO NOT MODIFY THE CODE BELOW THIS LINE----
@@ -13,7 +13,9 @@ void modify_grocery_list() {
     
     //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
     //----WRITE YOUR FUNCTION CALLS BELOW THIS LINE----
-    
+    print_grocery_list();
+    print_grocery_list(3);
+    print_grocery_list(5, 11);
 
 
 
@@ -22,7 +24,7 @@ void modify_grocery_list() {
     //----DO NOT MODIFY THE CODE BELOW THIS LINE----
 }
 
-void print_grocery_list(         ) { //----WRITE THE FUNCTION PARAMETER LIST WITHIN THE PARENTHESES
+void print_grocery_list(int apples, int oranges, int mangos) { //----WRITE THE FUNCTION PARAMETER LIST WITHIN THE PARENTHESES
                                      //----DO NOT MODIFY THE CODE BELOW THIS LINE----
     cout << apples << " apples" << "\n" << oranges << " oranges" << "\n" << mangos << " mangos" << "\n";
 }
