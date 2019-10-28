465b20f95c3207cd38b909fc11c558fac1bf2baa
nmctwisp <chen.k.yam@gmail.com>
2019-10-28 12:41:06 -0400

    updated implement methods example 1

--- Beginning C++ Programming - From Beginnner to Beyond/Section 13 - OOP - Classes and Objects/ImplementMethods1.cpp
+++ Beginning C++ Programming - From Beginnner to Beyond/Section 13 - OOP - Classes and Objects/ImplementMethods1.cpp
@@ -3,22 +3,6 @@
 
 using namespace std;
 
-class Player {
-
-private:
-    //attributes
-        string name;
-        int health;
-        int experience_points;
-public:
-    //methods
-        void talk(string message){
-
-            cout << name << " says " << message << endl;
-        }
-        bool is_dead();
-};
-
 class Account {
 
 private:
@@ -27,6 +11,7 @@ private:
         double balance;
 public:
     //methods
+        //Full Method Definition and Implementation within Class
         void set_balance(double bal){
             
             balance = bal;
@@ -35,12 +20,53 @@ public:
             
             return balance;
         }
-        void deposit(double amount){
 
-            balance += amount;
-        }
-        void withdraw(double amount){
-
-            balance -= amount;
-        }
+        //Prototype within class
+        //Methods will be declared outside the class declarations
+        void set_name(string input_name);
+        string get_name();
+        bool deposit(double amount);
+        bool withdraw(double amount);
 };
+
+void Account::set_name(string input_name){
+    name = input_name;
+}
+
+string Account::get_name(){
+    return name;
+}
+
+bool Account::deposit(double amount){
+    balance += amount;
+    return true;
+}
+
+bool Account::withdraw(double amount){
+    if (balance-amount >= 0){
+        balance -= amount;
+        return true;
+    }
+    else {
+        return false;
+    }
+
+}
+
+int main(){
+    Account frank_account;
+    frank_account.set_name("Frank's Account");
+    frank_account.set_balance(1000.00);
+
+    if (frank_account.deposit(200))
+        cout << "Deposit OK" << endl;
+    else
+        cout << "Deposit Not Allowed" << endl;
+
+    if (frank_account.withdraw(1500.00))
+        cout << "Sufficent Funds, OK to Withdraw" << endl;
+    else
+        cout << "Insufficent Funds" << endl;
+
+    return 0;
+}
