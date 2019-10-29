1294684d233c79a6e277502bba5a94da60d4d504
nmctwisp <chen.k.yam@gmail.com>
2019-10-29 12:05:19 -0400

    completed default parameter constructor example

---
+++ Beginning C++ Programming - From Beginnner to Beyond/Section 13 - OOP - Classes and Objects/DefaultConstructor.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Player{
+private:
+    // attributes
+        std::string name;
+        int health;
+        int xp;
+public:
+    // methods
+        void set_name(std::string name_val){
+            name = name_val;
+        }
+        std::string get_name(){
+            return name;
+        }
+    // Overloaded Constructors
+        Player(){// default arg, no arg constructor
+            name = "None";  
+            health = 100;
+            xp = 3;
+        }
+        Player(std::string name_val){// default arg mix one arg constructor
+            name = name_val;
+            health = 100;
+            xp = 3;
+        }
+        Player(std::string name_val, int health_val, int xp_val){
+            name = name_val;
+            health = health_val;
+            xp = xp_val;
+        }
+    // Destructor
+        ~Player(){
+        }
+};
+
+int main(){
+    Player hero;
+    Player frank = Player("Frank", 0, 13);
+    cout << frank.get_name() << endl;
+
+    return 0;
+}
