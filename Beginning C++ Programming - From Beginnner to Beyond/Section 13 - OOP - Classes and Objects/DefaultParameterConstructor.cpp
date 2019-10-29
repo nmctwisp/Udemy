68bddb495e5ce7e708d86ef0fe41ef4620a409d7
nmctwisp <chen.k.yam@gmail.com>
2019-10-29 14:01:11 -0400

    completed default parameters for constructors example

---
+++ Beginning C++ Programming - From Beginnner to Beyond/Section 13 - OOP - Classes and Objects/DefaultParameterConstructor.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Player{
+private:
+    // attributes
+    std::string name;
+    int health;
+    int xp;
+public:
+    // Constructor with default parameter values, the default is done on the definition, during implementation you can exclude the default values
+    // Player(); // including a no-args overload constructor would cause an error due to ambiguity 
+    Player(std::string name_val="None", int health_val=0, int xp_val=0);
+};
+
+Player::Player(std::string name_val, int health_val, int xp_val):
+    name{name_val},
+    health{health_val},
+    xp{xp_val}{
+        cout << "three-args constructor" << endl;
+}
+
+int main(){
+    Player empty;
+    Player frank = "Frank";
+    Player Hero = {"Hero", 100};
+    Player villian = {"Villian", 100, 55};
+}
