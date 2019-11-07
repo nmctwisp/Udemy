8c5c58c43bc3b939fee93d35fccda626c363336a
nmctwisp <chen.k.yam@gmail.com>
2019-11-07 09:40:19 -0500

    added GTTests project

---
+++ Beginning Test Driven Development in C++/GTTest/GTTest/GTTest.cpp
@@ -0,0 +1,11 @@
+// GTTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <gtest/gtest.h>
+
+
+TEST(GTTest, AssertTrue) {
+	ASSERT_TRUE(true);
+}
